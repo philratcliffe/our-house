85302931bb44092c86191e01f466e6497cebdddd
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-05-29 09:48:39 +0100

    Added a bootstrap4 theme

--- our_house/urls.py
+++ our_house/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.urls import path  # For django versions from 2.0 and up
+
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -34,3 +36,7 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
